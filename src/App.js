0ce49791b3c1a43ce3286a4a9137b28a0615c9af
Muhammad Salman <87569242+Salman-D@users.noreply.github.com>
2023-06-18 15:33:57 +0500

    chore(desigend) inproved

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React, { useState } from "react";
 import { jsPDF } from "jspdf";
 import html2canvas from "html2canvas";
-import "./print.css";
+import "./App.css";
 
 const App = () => {
   const [RegistrationPlateNoText, setRegistrationPlateNoText] = useState("");
   const [countyText, setCountyText] = useState("");
   const [selectedFont, setSelectedFont] = useState("Verdana");
+  const [topselectedFont, setTopSelectedFont] = useState("Arial");
   const [selectedBorder, setSelectedBorder] = useState("1mm");
   const [fontSize, setFontSize] = useState(50);
   const [topfontSize, setTopFontSize] = useState(16);
@@ -52,6 +53,10 @@ const App = () => {
     setSelectedFont(e.target.value);
   };
 
+  const handleTopFontChange = (e) => {
+    setTopSelectedFont(e.target.value);
+  };
+
   const handleBorderChange = (e) => {
     setSelectedBorder(e.target.value);
   };
@@ -64,72 +69,6 @@ const App = () => {
       setImageUrl(newImageUrl);
     }
   };
-// my old pdf 
-  // const handleDownloadPDF = () => {
-  //   const doc = new jsPDF({
-  //     orientation: "landscape",
-  //     unit: "px",
-  //     format: [520, 111],
-  //   });
-
-  //   const length = selectedBorder;
-  //   const numericLength = (selectedBorder === "none" ? 0 : length.match(/^(\d+)/)[0] )
-
-  //   const borderWidth = numericLength;
-
-  //   const canvas = document.createElement("canvas");
-  //   canvas.width = 520;
-  //   canvas.height = 111;
-  //   const context = canvas.getContext("2d");
-
-  //   // Draw the white background
-  //   context.fillStyle = "white";
-  //   context.fillRect(0, 0, 520, 111);
-
-  //   const logoImage = new Image();
-  //   logoImage.src = imageUrl;
-  //   logoImage.onload = () => {
-  //     // Add the border
-  //     context.strokeStyle = "black";
-  //     context.lineWidth = borderWidth;
-  //     context.strokeRect(
-  //       borderWidth / 2,
-  //       borderWidth / 2,
-  //       520 - borderWidth,
-  //       111 - borderWidth
-  //     );
-
-  //     // Add the logo image to the canvas
-  //     context.drawImage(logoImage, 0, 0, 57, 111);
-
-  //     const textX = 57; // Start position for text
-
-  //     context.font = `bold ${topfontSize}px ${selectedFont}`;
-  //     context.fillStyle = "black";
-  //     context.textAlign = "center"; // Center align the text
-  //     context.fillText(countyText, textX + 231, 25); // Center horizontally at x=288
-
-  //     context.font = `bold ${fontSize}px ${selectedFont}`;
-  //     context.fillText(RegistrationPlateNoText, textX + 231, 80); // Center horizontally at x=288
-
-  //     // Add the canvas as an image to the PDF
-  //     doc.addImage(
-  //       canvas.toDataURL("image/png"),
-  //       "PNG",
-  //       borderWidth / 2,
-  //       borderWidth / 2,
-  //       520 - borderWidth,
-  //       111 - borderWidth,
-  //       undefined,
-  //       "FAST",
-  //       0,
-  //       1200 // Set the DPI value to 600 for higher quality
-  //     );
-
-  //     // Save the PDF
-  //     doc.save("registration.pdf");
-  //   };
-  // };
 
   const handleDownloadmakePDF = () => {
     const divElement = document.getElementById("pdfDiv");
@@ -141,192 +80,218 @@ const App = () => {
       pdf.save("irl_plate.pdf");
     });
   };
-  // Windows print
-  // const handleWindowsDownloadPDF = () => {
-  //     window.print();
-  // };
 
   return (
     <div>
-      <div className="print">
+      <div className="container">
         <h1>Ireland Vehicle Registration Plate</h1>
       </div>
-
-      <div className="print">
-        <button onClick={handleChangeImage}>Change Image</button>
-      </div>
-
-      <div className="print">
-        <label>Registration Plate No.</label>
-        <input
-          type="text"
-          value={RegistrationPlateNoText}
-          onChange={handleRegistrationPlateNoTextChange}
-        />
-      </div>
-
-      <div className="print">
-        <label htmlFor="font-size-input">
-          Registration Plate Font Size: {fontSize}px
-        </label>
-        <input
-          type="range"
-          id="font-size-input"
-          min="10"
-          max="80"
-          value={fontSize}
-          onChange={handleFontSizeChange}
-        />
-      </div>
-
-      <div className="print">
-        <label>Country Text:</label>
-        <input
-          type="text"
-          value={countyText}
-          onChange={handleCountyTextChange}
-        />
-      </div>
-
-      <div className="print">
-        <label>Font:</label>
-        <select value={selectedFont} onChange={handleFontChange}>
-          <option value="Verdana">Verdana</option>
-          <option value="Arial">Arial</option>
-          <option value="Times New Roman">Times New Roman</option>
-          <option value="Courier New">Courier New</option>
-          <option value="Georgia">Georgia</option>
-          <option value="Tahoma">Tahoma</option>
-          <option value="Impact">Impact</option>
-          <option value="Comic Sans MS">Comic Sans MS</option>
-          <option value="Trebuchet MS">Trebuchet MS</option>
-          <option value="Arial Black">Arial Black</option>
-          <option value="Lucida Sans Unicode">Lucida Sans Unicode</option>
-          <option value="Palatino Linotype">Palatino Linotype</option>
-          <option value="Garamond">Garamond</option>
-          <option value="Bookman">Bookman</option>
-          <option value="Copperplate">Copperplate</option>
-          <option value="Franklin Gothic Medium">Franklin Gothic Medium</option>
-        </select>
-      </div>
-
-      <div className="print">
-        <label htmlFor="font-size-input">
-          Top Plate Font Size: {topfontSize}px
-        </label>
-        <input
-          type="range"
-          id="font-size-input"
-          min="10"
-          max="80"
-          value={topfontSize}
-          onChange={handleTopFontSizeChange}
-        />
-      </div>
-
-      <div className="print">
-        <label>Border Style:</label>
-        <select value={selectedBorder} onChange={handleBorderChange}>
-          <option value="none">none</option>
-          <option value="1mm">1mm</option>
-          <option value="2mm">2mm</option>
-          <option value="3mm">3mm</option>
-          <option value="4mm">4mm</option>
-          <option value="5mm">5mm</option>
-          <option value="6mm">6mm</option>
-          <option value="7mm">7mm</option>
-        </select>
-      </div>
-
-      <div className="print">
-        <button onClick={handleToggleImage}>Hide Image</button>
-      </div>
-
-      <div className="print">
-        <button onClick={handleCenterPlate}>Center Plate</button>
-      </div>
-
-      <div>
-        <div className="print">
-          <h2>Registration Plate Preview:</h2>
-        </div>
-        <div
-          id="pdfDiv"
-          style={{
-            border: `${
-              selectedBorder === "none"
-                ? "none"
-                : `${selectedBorder} solid black`
-            } `,
-            height: "111px",
-            width: "520px",
-            display: "flex",
-            justifyContent: "center",
-            alignItems: "center",
-            position: "relative",
-            margin: "auto",
-            borderRadius: "12px",
-          }}
-        >
-          {imageVisible && (
-            <div
-              style={{
-                width: "10%",
-                height: "100%",
-                position: "absolute",
-                backgroundColor:`${imageUrl === "blue_irl.png"?"#0f4add":"black"}`,
-                left: "0",
-              }}
-            >
-              <img
-                src={imageUrl}
-                alt="IRL"
-                style={{
-                  width: "100%",
-                  height: "100%",
-                  borderRadius: "8px",
-                }}
-              />
-            </div>
-          )}
+      <div
+        id="pdfDiv"
+        style={{
+          border: `${
+            selectedBorder === "none" ? "none" : `${selectedBorder} solid black`
+          } `,
+          height: "111px",
+          width: "520px",
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          position: "relative",
+          margin: "auto",
+          borderRadius: "12px",
+          backgroundColor:"white"
+        }}
+      >
+        {imageVisible && (
           <div
             style={{
-              marginLeft: `${centerPlate}`,
-              display: "flex",
-              flexDirection: "column",
-              alignItems: "center",
+              width: "10%",
+              height: "100%",
+              position: "absolute",
+              backgroundColor: `${
+                imageUrl === "blue_irl.png" ? "#0f4add" : "black"
+              }`,
+              left: "0",
             }}
           >
-            <span
+            <img
+              src={imageUrl}
+              alt="IRL"
               style={{
-                fontSize: `${topfontSize}px`,
-                fontFamily: `${selectedFont}`,
-                fontWeight: "bold",
+                width: "100%",
+                height: "100%",
+                borderRadius: "8px",
               }}
-            >
-              {countyText}
-            </span>
-            <span
-              style={{
-                fontSize: `${fontSize}px`,
-                fontFamily: `${selectedFont}`,
-                fontWeight: "bold",
-              }}
-            >
-              {RegistrationPlateNoText}
-            </span>
+            />
           </div>
+        )}
+        <div
+          style={{
+            marginLeft: `${centerPlate}`,
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+          }}
+        >
+          <span
+            style={{
+              fontSize: `${topfontSize}px`,
+              fontFamily: `${topselectedFont}`,
+              fontWeight: "bold",
+            }}
+          >
+            {countyText}
+          </span>
+          <span
+            style={{
+              fontSize: `${fontSize}px`,
+              fontFamily: `${selectedFont}`,
+              fontWeight: "bold",
+            }}
+          >
+            {RegistrationPlateNoText}
+          </span>
         </div>
       </div>
-      <div className="print">
-        <button onClick={handleDownloadmakePDF}>Download  PDF</button>
+
+      <div className="container">
+        <div className="container-child">
+          <label>Registration Plate No:</label>
+          <input
+            type="text"
+            value={RegistrationPlateNoText}
+            onChange={handleRegistrationPlateNoTextChange}
+          />
+        </div>
+
+        <div className="container-child">
+          <label>Registration Plate Font:</label>
+          <select value={selectedFont} onChange={handleFontChange}>
+            <option value="Verdana">Verdana</option>
+            <option value="Arial">Arial</option>
+            <option value="Times New Roman">Times New Roman</option>
+            <option value="Courier New">Courier New</option>
+            <option value="Georgia">Georgia</option>
+            <option value="Tahoma">Tahoma</option>
+            <option value="Impact">Impact</option>
+            <option value="Comic Sans MS">Comic Sans MS</option>
+            <option value="Trebuchet MS">Trebuchet MS</option>
+            <option value="Arial Black">Arial Black</option>
+            <option value="Lucida Sans Unicode">Lucida Sans Unicode</option>
+            <option value="Palatino Linotype">Palatino Linotype</option>
+            <option value="Garamond">Garamond</option>
+            <option value="Bookman">Bookman</option>
+            <option value="Copperplate">Copperplate</option>
+            <option value="Franklin Gothic Medium">
+              Franklin Gothic Medium
+            </option>
+          </select>
+        </div>
+
+        <div className="container-child">
+          <label htmlFor="font-size-input">
+            Registration Plate Font Size: {fontSize}px
+          </label>
+          <input
+            type="range"
+            id="font-size-input"
+            min="10"
+            max="80"
+            value={fontSize}
+            onChange={handleFontSizeChange}
+          />
+        </div>
       </div>
-      {/* <div className="print">
-        <button onClick={handleDownloadPDF}>Exact Size Download PDF</button>
+
+      <div className="container">
+        <div className="container-child">
+          <label>Top Text:</label>
+          <input
+            type="text"
+            value={countyText}
+            onChange={handleCountyTextChange}
+          />
+        </div>
+
+        <div className="container-child">
+          <label>Top Font:</label>
+          <select value={topselectedFont} onChange={handleTopFontChange}>
+            <option value="Arial">Arial</option>
+            <option value="Verdana">Verdana</option>
+            <option value="Times New Roman">Times New Roman</option>
+            <option value="Courier New">Courier New</option>
+            <option value="Georgia">Georgia</option>
+            <option value="Tahoma">Tahoma</option>
+            <option value="Impact">Impact</option>
+            <option value="Comic Sans MS">Comic Sans MS</option>
+            <option value="Trebuchet MS">Trebuchet MS</option>
+            <option value="Arial Black">Arial Black</option>
+            <option value="Lucida Sans Unicode">Lucida Sans Unicode</option>
+            <option value="Palatino Linotype">Palatino Linotype</option>
+            <option value="Garamond">Garamond</option>
+            <option value="Bookman">Bookman</option>
+            <option value="Copperplate">Copperplate</option>
+            <option value="Franklin Gothic Medium">
+              Franklin Gothic Medium
+            </option>
+          </select>
+        </div>
+
+        <div className="container-child">
+          <label htmlFor="font-size-input">
+            Top Plate Font Size: {topfontSize}px
+          </label>
+          <input
+            type="range"
+            id="font-size-input"
+            min="10"
+            max="80"
+            value={topfontSize}
+            onChange={handleTopFontSizeChange}
+          />
+        </div>
+
+        <div className="container-child">
+          <label>Border Style:</label>
+          <select value={selectedBorder} onChange={handleBorderChange}>
+            <option value="none">none</option>
+            <option value="1mm">1mm</option>
+            <option value="2mm">2mm</option>
+            <option value="3mm">3mm</option>
+            <option value="4mm">4mm</option>
+            <option value="5mm">5mm</option>
+            <option value="6mm">6mm</option>
+            <option value="7mm">7mm</option>
+          </select>
+        </div>
       </div>
-      <div className="print">
-        <button onClick={handleWindowsDownloadPDF}>Download PDF</button>
-      </div> */}
+
+      <div className="container">
+        <div className="container-child">
+          <button className="button" onClick={handleToggleImage}>
+            Hide Image
+          </button>
+        </div>
+
+        <div className="container-child">
+          <button className="button" onClick={handleCenterPlate}>
+            Center Plate
+          </button>
+        </div>
+
+        <div className="container-child">
+          <button className="button" onClick={handleDownloadmakePDF}>
+            Download PDF
+          </button>
+        </div>
+
+        <div className="container-child">
+          <button className="button" onClick={handleChangeImage}>
+            Change Image
+          </button>
+        </div>
+      </div>
     </div>
   );
 };
